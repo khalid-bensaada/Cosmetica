83fea92ca036c1a42538371e4f6705075a3609e3
khalid-bensaada <kbensaada953@gmail.com>
2026-03-17 14:01:52 +0000

    added a routes in file api.php

---
+++ routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login',    [AuthController::class, 'login']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('me',      [AuthController::class, 'me']);
+    });
+});
+
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('orders', [OrderController::class, 'store']);
+});
+
+
+
+Route::get('products',        [ProductController::class, 'index']);
+Route::get('products/{slug}', [ProductController::class, 'show']);
+
+
+Route::middleware(['auth:api', 'role:admin'])->group(function () {
+    Route::post('products',           [ProductController::class, 'store']);
+    Route::put('products/{product}',  [ProductController::class, 'update']);
+    Route::delete('products/{product}', [ProductController::class, 'destroy']);
+});
+
+
+
+
+
+Route::get('categories', [CategoryController::class, 'index']);
+
+Route::middleware(['auth:api', 'role:admin'])->group(function () {
+    Route::post('categories',             [CategoryController::class, 'store']);
+    Route::put('categories/{category}',   [CategoryController::class, 'update']);
+    Route::delete('categories/{category}',[CategoryController::class, 'destroy']);
+});
