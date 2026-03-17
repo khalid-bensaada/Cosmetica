06fe76c9783a90fad66272897fdcf0a79854ce5b
khalid-bensaada <kbensaada953@gmail.com>
2026-03-17 13:56:42 +0000

    added a models of product

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Product extends Model
+{
+    use HasSlug;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'category_id',
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
